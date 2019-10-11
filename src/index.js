5b197789a3747f67167288969221474de2d40738
Diego Alejandro Reyes Espitia <diego.reyes@tars.dev>
2019-10-11 08:35:15 -0500

    Modificacion en este branch

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+const element = <h1>Hello, Platzi Badges Diego!</h1>;
 
 const container = document.getElementById('app');
 
